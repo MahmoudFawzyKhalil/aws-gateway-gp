9fb0f5fd7c0731771eb16865b87135f07711e995
Mohamed Alaa <84425503+Benalaa9528@users.noreply.github.com>
2022-05-30 23:16:08 +0200

    Create Configuration, Model,GateWay Packages To Service Layer

---
+++ src/main/java/eg/gov/iti/jets/service/model/TrackModel.java
@@ -0,0 +1,15 @@
+package eg.gov.iti.jets.service.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Data
+@Entity
+@Table(name = "tracks")
+public class TrackModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String trackName;
+
+}
