9b1fe687bf5b9edea7475a3ac767bdbf0ac5b447
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-02 13:36:17 -0700

    Add Instance Controller EndPoint

--- src/main/java/eg/gov/iti/jets/api/resource/instance/InstanceController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/instance/InstanceController.java
@@ -1,15 +1,16 @@
 package eg.gov.iti.jets.api.resource.instance;
 
 
+import eg.gov.iti.jets.service.management.InstanceManagement;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/instances")
-public class InstanceManagement {
+public class InstanceController {
 
     private final InstanceManagement instanceManagement;
 
-    public InstanceManagement(InstanceManagement instanceManagement) {
+    public InstanceController(InstanceManagement instanceManagement) {
         this.instanceManagement = instanceManagement;
     }
 
@@ -39,8 +40,30 @@ public class InstanceManagement {
     }
 
 
+    @PostMapping("instnace/assign")
+    void assignInstance(){
+        instanceManagement.assignInstance();
+    }
 
+    @PostMapping("customInstance/create")
+    void createInstanceUsingTemplate(){
+        instanceManagement.createInstanceUsingTemplate();
+    }
 
+    @GetMapping("instance/{id}")
+    void getInstance(@PathVariable String id){
+        instanceManagement.getInstance(id);
+    }
+
+    @GetMapping("instance/start/{id}")
+    void startInstance(@PathVariable String id){
+        instanceManagement.startInstance(id);
+    }
+
+    @GetMapping("instance/stop/{id}")
+    void stopInstance(@PathVariable String id){
+        instanceManagement.stopInstance(id);
+    }
 
 
 
