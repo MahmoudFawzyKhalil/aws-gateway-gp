ba53065bf4d545f8ef56e220a8547ad8fb5fdcac
Hala Tarek <halatarek821@gmail.com>
2022-06-02 13:26:12 +0200

    Create Instance Management Service

---
+++ src/main/java/eg/gov/iti/jets/service/management/InstanceManagement.java
@@ -0,0 +1,28 @@
+package eg.gov.iti.jets.service.management;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstanceManagement {
+
+    public void createInstances(){}
+
+    public void deleteInstances(){}
+
+    public void getAllInstances(){}
+
+    //filtering by branch
+    public void getAllBranchInstances(String branch){}
+
+    public void getAllTrackInstances(String Track){}
+
+
+
+
+
+
+
+
+
+
+}
