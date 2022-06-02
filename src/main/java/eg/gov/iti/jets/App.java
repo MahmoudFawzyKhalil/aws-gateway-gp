fabfe1512b247b2b51a4c8f71e336654b0707ab9
marwaayosiif <marwaayosiif@gmail.com>
2022-06-02 09:31:56 +0200

    csv method

--- src/main/java/eg/gov/iti/jets/App.java
+++ src/main/java/eg/gov/iti/jets/App.java
@@ -1,12 +1,23 @@
 package eg.gov.iti.jets;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
 
         SpringApplication.run(App.class, args);
+
     }
 }
+
+
+
--- src/main/java/eg/gov/iti/jets/service/management/StudentManagement.java
+++ src/main/java/eg/gov/iti/jets/service/management/StudentManagement.java
@@ -5,35 +5,52 @@ import eg.gov.iti.jets.service.model.Branch;
 import eg.gov.iti.jets.service.model.Student;
 import org.springframework.stereotype.Service;
 
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StudentManagement {
 
-    public Boolean createStudent( Student student){
+    public Boolean createStudent( Student student ) {
         return null;
     }
 
-    public Student updateStudent( Student student ){
+    public Student updateStudent( Student student ) {
         return null;
     }
 
-    public Boolean deleteStudent ( int id){
+    public Boolean deleteStudent( int id ) {
         return null;
     }
 
-    public List<Student> getAllStudents(){
+    public List<Student> getAllStudents() {
         return null;
     }
 
-    public Student getStudentById( int id ){
+    public Student getStudentById( int id ) {
         return null;
     }
 
-    public Boolean readFromCSV ( InputStream inputStream ){
+    public Boolean createStudentFromCSV( String csvFile ) {
+        List<String> strings = readCsvFile( csvFile );
 
         return null;
+    }
+
+    private List<String> readCsvFile( String csvFile ) {
+        final String delimiter = ",";
+        List<String> tempArr = new ArrayList<>();
+        try ( var br = new BufferedReader( new FileReader( csvFile ) ) ) {
+            String line;
+
+            while ( ( line = br.readLine() ) != null ) {
+                tempArr.add( line ) ;
+            }
+        } catch ( IOException ioe ) {
+            ioe.printStackTrace();
+        }
+        return tempArr;
     }
 
     //TODO checked if in track before
