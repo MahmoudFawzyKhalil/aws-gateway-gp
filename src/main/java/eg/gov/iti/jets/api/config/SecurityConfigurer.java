a47f600ae6bfa5c92a7a40ec74f083ff313ec06e
Ahmad Ashrf <ahmdashrf0097@gmail.com>
2022-06-03 15:25:40 +0200

    authorization configuration test

--- src/main/java/eg/gov/iti/jets/api/config/SecurityConfigurer.java
+++ src/main/java/eg/gov/iti/jets/api/config/SecurityConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
@@ -34,8 +36,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/authenticate")
-                .permitAll()
+                .mvcMatchers("/api/admin").hasAuthority("WRITE")
+                .mvcMatchers("/api/hello").hasAuthority("READ")
+                .mvcMatchers("/api/users").hasAnyAuthority("WRITE","READ")
+                .mvcMatchers("/api/authenticate").permitAll()
                 .anyRequest()
                 .authenticated();
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
