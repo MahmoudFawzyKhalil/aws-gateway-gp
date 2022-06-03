01907b2e689b528a9951561f6ecb7fb6b4079eb7
marwaayosiif <marwaayosiif@gmail.com>
2022-06-03 11:26:42 +0200

    merging with rest of endpoints

--- src/main/java/eg/gov/iti/jets/api/util/Mapper.java
+++ src/main/java/eg/gov/iti/jets/api/util/Mapper.java
@@ -2,18 +2,12 @@ package eg.gov.iti.jets.api.util;
 
 import eg.gov.iti.jets.api.resource.branch.BranchRequest;
 import eg.gov.iti.jets.api.resource.branch.BranchResponse;
-import eg.gov.iti.jets.api.resource.instructor.InstructorRequest;
-import eg.gov.iti.jets.api.resource.instructor.InstructorResponse;
 import eg.gov.iti.jets.api.resource.intake.IntakeRequest;
 import eg.gov.iti.jets.api.resource.intake.IntakeResponse;
-import eg.gov.iti.jets.api.resource.student.StudentRequest;
-import eg.gov.iti.jets.api.resource.student.StudentResponse;
 import eg.gov.iti.jets.api.resource.supervisor.SupervisorRequest;
 import eg.gov.iti.jets.api.resource.supervisor.SupervisorResponse;
 import eg.gov.iti.jets.api.resource.track.TrackRequest;
 import eg.gov.iti.jets.api.resource.track.TrackResponse;
-import eg.gov.iti.jets.api.resource.trainingManager.TrainingManagerRequest;
-import eg.gov.iti.jets.api.resource.trainingManager.TrainingManagerResponse;
 import eg.gov.iti.jets.api.resource.trainingProgram.TrainingProgramRequest;
 import eg.gov.iti.jets.api.resource.trainingProgram.TrainingProgramResponse;
 import eg.gov.iti.jets.service.model.*;
@@ -37,28 +31,6 @@ public class Mapper {
     }
 
 
-    public TrainingManager mapFromTrainingManagerRequestToTrainingManager( TrainingManagerRequest trainingManagerRequest){
-        return null;
-    }
-
-    public TrainingManagerResponse mapFromTrainingManagerToTrainingManagerResponse( TrainingManager trainingManager){
-        return null;
-    }
-    public Instructor mapFromInstructorRequestToInstructor( InstructorRequest InstructorRequest){
-        return null;
-    }
-
-    public InstructorResponse mapFromInstructorToInstructorResponse( Instructor instructor){
-        return null;
-    }
-
-    public Student mapFromStudentRequestToStudent( StudentRequest studentRequest){
-        return null;
-    }
-
-    public StudentResponse mapFromStudentToStudentResponse( Student student){
-        return null;
-    }
 
     public IntakeResponse mapFromIntakeToIntakeResponse(Intake intake) {
         return null;
