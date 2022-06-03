7dc2dd88debdafe46fce48ce6cfdca956297715b
Mohamed-Elsisi <elsisi901@gmail.com>
2022-06-03 08:31:20 +0200

    add Track Dao, Repo and  Implementation

---
+++ src/main/java/eg/gov/iti/jets/persistence/dao/impls/TrackDaoImpl.java
@@ -0,0 +1,50 @@
+package eg.gov.iti.jets.persistence.dao.impls;
+
+import eg.gov.iti.jets.persistence.dao.TrackDao;
+import eg.gov.iti.jets.persistence.entity.Track;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TrackDaoImpl implements TrackDao {
+    private final TrackRepo trackRepo;
+
+    public TrackDaoImpl(TrackRepo trackRepo) {
+        this.trackRepo = trackRepo;
+    }
+
+    @Override
+    public Track save(Track entity) {
+        return trackRepo.save(entity);
+    }
+
+    @Override
+    public Track update(Track entity) {
+        return trackRepo.save(entity);
+    }
+
+    @Override
+    public Optional<Track> findById(Integer integer) {
+        return trackRepo.findById(integer);
+    }
+
+    @Override
+    public List<Track> findAll() {
+        return trackRepo.findAll();
+    }
+
+    @Override
+    public List<Track> findAll(int pageNumber, int pageSize) {
+        Page<Track> trackPage = trackRepo.findAll(PageRequest.of(pageNumber, pageSize));
+        return trackPage.getContent();
+    }
+
+    @Override
+    public List<Track> findAllByExample(Track example) {
+        return null;
+    }
+}
