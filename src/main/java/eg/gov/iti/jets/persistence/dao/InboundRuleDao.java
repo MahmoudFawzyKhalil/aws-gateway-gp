d2f9ca9ab1eb8c1bb4dc96ecf7086bab765f46be
Sondos Salah <sondoss.salahh@gmail.com>
2022-06-04 14:49:32 +0200

    Add InboundRuleDao and OutboundRuleDao

---
+++ src/main/java/eg/gov/iti/jets/persistence/dao/InboundRuleDao.java
@@ -0,0 +1,6 @@
+package eg.gov.iti.jets.persistence.dao;
+
+import eg.gov.iti.jets.persistence.entity.aws.InboundRule;
+
+public interface InboundRuleDao extends GenericCrudDao<InboundRule,Integer>{
+}
