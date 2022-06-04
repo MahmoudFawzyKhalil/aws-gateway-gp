168e43156d3885389e644b253e28840f28df5245
AhmedOsama <ahmedosamam03@gmail.com>
2022-06-04 09:42:03 +0200

    Add @ Repository to AmiDAoImpl

--- src/main/java/eg/gov/iti/jets/persistence/dao/impls/AmiDaoIml.java
+++ src/main/java/eg/gov/iti/jets/persistence/dao/impls/AmiDaoIml.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class AmiDaoIml implements AmiDao {
 
     private final AmiRepo amiRepo;
