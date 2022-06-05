8ccbc0823e6fea4641e9b2299ab8301421112716
Ahmad Ashrf <ahmdashrf0097@gmail.com>
2022-06-05 21:25:49 +0200

    adding security authorities for methods

--- src/main/java/eg/gov/iti/jets/api/resource/template/TemplateController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/template/TemplateController.java
@@ -5,14 +5,14 @@ import eg.gov.iti.jets.persistence.entity.aws.Ami;
 import eg.gov.iti.jets.persistence.entity.aws.SecurityGroup;
 import eg.gov.iti.jets.persistence.entity.aws.TemplateConfiguration;
 import eg.gov.iti.jets.service.management.TemplateManagement;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import eg.gov.iti.jets.service.model.UserAdapter;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/api/template")
@@ -27,17 +27,20 @@ public class TemplateController {
     }
 
     @PostMapping
+    @Secured("MANAGE_TEMPLATE")
     public SuccessResponse createTemplate(@RequestBody TemplateRequest templateRequest ){
        return new SuccessResponse(templateManagement.createTemplate(mapper.mapFromTemplateRequestToTemplateConfig(templateRequest)));
     }
 
     @DeleteMapping("/{id}")
+    @Secured("MANAGE_TEMPLATE")
     public SuccessResponse deleteTemplate ( @PathVariable int id ){
         return new SuccessResponse(templateManagement.deleteTemplate( id )) ;
     }
 
 
     @GetMapping
+    @Secured("VIEW_TEMPLATES")
     // TODO: 6/5/2022 get the Id?
     public TemplateViewResponse getAllTemplates(){
         List<TemplateResponse> templateResponses = new ArrayList<>();
@@ -52,23 +55,27 @@ public class TemplateController {
     }
 
     @GetMapping("/subnet")
+    @Secured("MANAGE_TEMPLATE")
     SubnetResponse getAllSubnet(){
         return  mapper.mapFromSubnetToSubnetResponse(templateManagement.getAllSubnet());
 
     }
 
     @PostMapping("/ami")
+    @Secured("MANAGE_TEMPLATE")
     public AmiViewResponse getAmi(@RequestBody AmiRequest amiRequest){
         Optional<Ami> ami = templateManagement.describeAmi( amiRequest.getAmiId() );
         return ami.map( value -> new AmiViewResponse( true, mapper.mapFromAmiToAmiResponse( value ) ) ).orElseGet( () -> new AmiViewResponse( false, null ) );
     }
 
-    @GetMapping("types")
+    @GetMapping("/types")
+    @Secured("MANAGE_TEMPLATE")
     InstanceTypeResponse getInstanceTypes(){
         return  new InstanceTypeResponse(templateManagement.getInstanceTypes());
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/securitygroup/{id}")
+    @Secured("MANAGE_TEMPLATE")
     SecurityGroupObjectResponse getSecurityGroups(@PathVariable String id){
         List<SecurityGroup> securityGroups= templateManagement.describeSecurityGroupsForVpc(id);
         List<SecurityGroupResponse> securityGroupResponses = new ArrayList<>();
