e2a036454ce57401bb4cdba27c9951e13ee89c6c
amiraElmergawy <amiraelmergawy@gmail.com>
2022-06-05 16:17:00 +0200

    Add findLastStopDate method

--- src/main/java/eg/gov/iti/jets/persistence/dao/InstanceLogsDao.java
+++ src/main/java/eg/gov/iti/jets/persistence/dao/InstanceLogsDao.java
@@ -1,6 +1,11 @@
 package eg.gov.iti.jets.persistence.dao;
 
+import eg.gov.iti.jets.persistence.entity.aws.Instance;
 import eg.gov.iti.jets.persistence.entity.aws.InstanceLogs;
 
+import java.time.LocalDateTime;
+
 public interface InstanceLogsDao extends GenericCrudDao<InstanceLogs,Long> {
+
+    LocalDateTime findLastStopDate(Instance instance);
 }
--- src/main/java/eg/gov/iti/jets/persistence/dao/impls/InstanceLogsRepo.java
+++ src/main/java/eg/gov/iti/jets/persistence/dao/impls/InstanceLogsRepo.java
@@ -1,7 +1,14 @@
 package eg.gov.iti.jets.persistence.dao.impls;
 
+import eg.gov.iti.jets.persistence.entity.aws.Instance;
 import eg.gov.iti.jets.persistence.entity.aws.InstanceLogs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 interface InstanceLogsRepo extends JpaRepository<InstanceLogs,Long> {
+    @Query("select i.dateTime from InstanceLogs i where i.action = 'stop' and i.instance = :instance")
+    LocalDateTime findLastStopDate(@Param("instance") Instance instance);
 }
