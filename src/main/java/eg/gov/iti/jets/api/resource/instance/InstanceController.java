acdf5fd06b332b42716eb598456e734311fa2c9c
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-05 08:32:58 -0700

    add get all subnet and describe Ami endpoint

--- src/main/java/eg/gov/iti/jets/api/resource/instance/InstanceController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/instance/InstanceController.java
@@ -1,10 +1,13 @@
 package eg.gov.iti.jets.api.resource.instance;
 
 
+import eg.gov.iti.jets.persistence.entity.aws.Ami;
+import eg.gov.iti.jets.persistence.entity.aws.Subnet;
 import eg.gov.iti.jets.service.management.impl.InstanceManagementImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/instances")
@@ -70,7 +73,18 @@ public class InstanceController {
 
     @GetMapping("types")
     List<String> getInstanceTypes(){
-            return  instanceManagement.getInstanceTypes();
+        return  instanceManagement.getInstanceTypes();
+    }
+
+    @GetMapping("subnet")
+    List<Subnet> getAllSubnet(){
+        return  instanceManagement.getAllSubnet();
+    }
+
+
+    @GetMapping("ami/{id}")
+    Optional<Ami> describeAmi (@PathVariable String id){
+        return  instanceManagement.describeAmi(id);
     }
 
 }
