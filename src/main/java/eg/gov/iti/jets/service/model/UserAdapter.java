ae40a8a7807147a862952c3b77e4199905ede16c
Ahmad Ashrf <ahmdashrf0097@gmail.com>
2022-06-05 20:07:59 +0200

    userAdapter now have getId method , to be loaded onto jwt and also authorities field

--- src/main/java/eg/gov/iti/jets/service/model/UserAdapter.java
+++ src/main/java/eg/gov/iti/jets/service/model/UserAdapter.java
@@ -3,22 +3,39 @@ package eg.gov.iti.jets.service.model;
 import eg.gov.iti.jets.persistence.entity.Privilege;
 import eg.gov.iti.jets.persistence.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import java.util.Collection;
+import org.springframework.util.Assert;
+
+import java.io.Serializable;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-public class UserAdapter implements UserDetails {
+public class UserAdapter implements UserDetails , Serializable {
+    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
     private final User user;
+
+    private Set<GrantedAuthority> authorities; // todo let it be final
+
+    public UserAdapter(User user , Collection<? extends GrantedAuthority> authorities ){
+
+        this.user = user;
+        this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getRole().getPrivileges().stream()
-                .map(Privilege::getName)
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+//        return user.getRole().getPrivileges().stream()
+//                .map(Privilege::getName)
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+        return authorities;
     }
 
     @Override
@@ -65,5 +82,31 @@ public class UserAdapter implements UserDetails {
          * user entity should provide this property
          */
         return true;
+    }
+    public Integer getId(){
+        return user.getId();
+    }
+
+    private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+        // Ensure array iteration order is predictable (as per
+        // UserDetails.getAuthorities() contract and SEC-717)
+        SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<>(new Comparator<GrantedAuthority>() {
+            @Override
+            public int compare(GrantedAuthority o1, GrantedAuthority o2) {
+                if (o2.getAuthority() == null) {
+                    return -1;
+                }
+                if (o1.getAuthority() == null) {
+                    return 1;
+                }
+                return o1.getAuthority().compareTo(o2.getAuthority());
+            }
+        });
+        for (GrantedAuthority grantedAuthority : authorities) {
+            Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+            sortedAuthorities.add(grantedAuthority);
+        }
+        return sortedAuthorities;
     }
 }
