622b8714795c3a5ae5ad55f153ad83353f085086
Hala Tarek <halatarek821@gmail.com>
2022-06-07 09:33:52 +0200

    Create TrainingProgram Cruds

---
+++ src/main/java/eg/gov/iti/jets/service/management/TrainingProgramManagement.java
@@ -0,0 +1,18 @@
+package eg.gov.iti.jets.service.management;
+
+import eg.gov.iti.jets.persistence.entity.TrainingProgram;
+
+import java.util.List;
+
+public interface TrainingProgramManagement {
+
+    public Boolean createTrainingProgram( TrainingProgram trainingProgram);
+
+    public TrainingProgram updateTrainingProgram( TrainingProgram trainingProgram );
+
+    public Boolean deleteTrainingProgram ( int id);
+
+    public List<TrainingProgram> getAllTrainingPrograms();
+
+    public TrainingProgram getTrainingProgramById( int id );
+}
