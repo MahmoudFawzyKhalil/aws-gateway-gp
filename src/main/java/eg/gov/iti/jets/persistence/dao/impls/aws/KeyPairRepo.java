6569d6f64480d2b8f6b3e53fb6e7b2b4ae967093
amiraElmergawy <amiraelmergawy@gmail.com>
2022-06-07 15:43:01 +0200

    update keypairdaoimple for projection and throw null pointer exception

--- src/main/java/eg/gov/iti/jets/persistence/dao/impls/aws/KeyPairRepo.java
+++ src/main/java/eg/gov/iti/jets/persistence/dao/impls/aws/KeyPairRepo.java
@@ -1,7 +1,19 @@
 package eg.gov.iti.jets.persistence.dao.impls.aws;
 
 import eg.gov.iti.jets.persistence.entity.aws.KeyPair;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 interface KeyPairRepo extends JpaRepository<KeyPair,Integer> {
+    <C> Optional<C> findById(Integer id, Class<C> projection);
+
+
+    <C> Page<C> findBy(PageRequest pageRequest, Class<C> projection);
+
+    <C> List<C> findAllBy(Example<C> example, Class<C> projection);
 }
