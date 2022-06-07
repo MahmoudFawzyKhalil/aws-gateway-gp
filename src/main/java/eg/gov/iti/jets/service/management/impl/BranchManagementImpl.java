2bee799efab367152601b7291601349dc737c3ce
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-06 23:04:42 -0700

    add Branch Management methods

--- src/main/java/eg/gov/iti/jets/service/management/impl/BranchManagementImpl.java
+++ src/main/java/eg/gov/iti/jets/service/management/impl/BranchManagementImpl.java
@@ -1,36 +1,38 @@
 package eg.gov.iti.jets.service.management.impl;
 
+import eg.gov.iti.jets.persistence.dao.BranchDao;
 import eg.gov.iti.jets.persistence.entity.Branch;
 import eg.gov.iti.jets.service.management.BranchManagement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BranchManagementImpl implements BranchManagement {
+
+    @Autowired
+    BranchDao branchDao;
+
     @Override
-    public Boolean createBranch( Branch branch ) {
-        return null;
+    public Branch createBranch( Branch branch ) {
+        return branchDao.save(branch);
     }
 
     @Override
     public Branch updateBranch( Branch branch ) {
-        return null;
-    }
-
-    @Override
-    public Boolean deleteBranch( int id ) {
-        return null;
+        return branchDao.save(branch);
     }
 
     @Override
     public List<Branch> getAllBranches() {
-        return null;
+        return branchDao.findAll();
     }
 
     @Override
-    public Branch getBranchById( int id ) {
-        return null;
+    public Optional<Branch> getBranchById(int id ) {
+        return branchDao.findById(id);
     }
 
 }
