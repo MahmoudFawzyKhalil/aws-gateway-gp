820684d72da7fa3b8a92fa6c4e9cf87ccac940bc
marwaayosiif <marwaayosiif@gmail.com>
2022-06-07 13:53:28 +0200

    getInstances function

--- src/main/java/eg/gov/iti/jets/api/resource/instance/InstanceController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/instance/InstanceController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -67,6 +68,11 @@ public class InstanceController {
         Instance instance = instanceManagement.instanceDetails( instanceId );
 
         return mapper.mapFromInstanceToInstanceResponse( instance );
+    }
+
+    @GetMapping
+    Boolean getInstances ( @AuthenticationPrincipal UserDetails userDetails ){
+        return null;
     }
 
 }
