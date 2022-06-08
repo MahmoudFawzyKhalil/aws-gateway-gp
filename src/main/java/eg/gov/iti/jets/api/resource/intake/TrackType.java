c1d59b0070c49dcf87e67ed928a5dc54a4d1ed2c
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-08 09:34:50 -0700

    add Track type response

---
+++ src/main/java/eg/gov/iti/jets/api/resource/intake/TrackType.java
@@ -0,0 +1,14 @@
+package eg.gov.iti.jets.api.resource.intake;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TrackType {
+    int id;
+    String name;
+}
