5f9e2bb63abf0f4c16cf46409aa0a2773f221d7c
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-07 19:40:51 -0700

    add Intake Management methods

---
+++ src/main/java/eg/gov/iti/jets/service/management/IntakeManagement.java
@@ -0,0 +1,16 @@
+package eg.gov.iti.jets.service.management;
+
+import eg.gov.iti.jets.persistence.entity.Intake;
+import java.util.List;
+import java.util.Optional;
+
+public interface IntakeManagement {
+    Intake createIntake(Intake intake );
+
+    Intake updateIntake( Intake intake );
+
+    List<Intake> getAllIntakes();
+
+    Optional<Intake> getIntakeById(int id);
+
+}
--- src/main/java/eg/gov/iti/jets/service/management/impl/IntakeManagementImpl.java
+++ src/main/java/eg/gov/iti/jets/service/management/impl/IntakeManagementImpl.java
@@ -1,36 +1,54 @@
 package eg.gov.iti.jets.service.management.impl;
 
+import eg.gov.iti.jets.persistence.dao.BranchDao;
+import eg.gov.iti.jets.persistence.dao.IntakeDao;
 import eg.gov.iti.jets.persistence.entity.Intake;
 import eg.gov.iti.jets.service.management.CrudOperations;
+import eg.gov.iti.jets.service.management.IntakeManagement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class IntakeManagementImpl implements CrudOperations<Intake> {
+public class IntakeManagementImpl implements IntakeManagement {
+
+    @Autowired
+    IntakeDao intakeDao;
 
     @Override
-    public Boolean create( Intake type) {
-        return null;
+    public Intake createIntake(Intake intake) {
+        return intakeDao.save(intake);
     }
 
     @Override
-    public Intake update(Intake type) {
-        return null;
+    public Intake updateIntake(Intake intake) {
+        return intakeDao.update(intake);
     }
 
     @Override
-    public Boolean delete(int id) {
-        return null;
+    public List<Intake> getAllIntakes() {
+        return intakeDao.findAll();
     }
 
     @Override
-    public List<Intake> getAll() {
-        return null;
+    public Optional<Intake> getIntakeById(int id) {
+        return intakeDao.findById(id);
     }
 
-    @Override
-    public Intake getById(int id) {
-        return null;
-    }
+//    @Override
+//    public Optional<Intake> getUserInIntakeById(int id) {
+//        Optional<Intake> intake = intakeDao.findById(id);
+//        User User = userDao.
+//    }
+
+
+
+//    @Override
+//    public Boolean delete(int id) {
+//        return null;
+//    }
+
+
 }
