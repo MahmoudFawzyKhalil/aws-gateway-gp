1d3b14de155efec2877b0a38ac0c92bc673fd7ae
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-07 19:42:06 -0700

    add get by id methods

--- src/main/java/eg/gov/iti/jets/service/util/MapperUtilForApi.java
+++ src/main/java/eg/gov/iti/jets/service/util/MapperUtilForApi.java
@@ -1,9 +1,9 @@
 package eg.gov.iti.jets.service.util;
 
-import eg.gov.iti.jets.persistence.dao.BranchDao;
-import eg.gov.iti.jets.persistence.dao.SecurityGroupDao;
-import eg.gov.iti.jets.persistence.dao.UserDao;
+import eg.gov.iti.jets.persistence.dao.*;
 import eg.gov.iti.jets.persistence.entity.Branch;
+import eg.gov.iti.jets.persistence.entity.Intake;
+import eg.gov.iti.jets.persistence.entity.TrainingProgram;
 import eg.gov.iti.jets.persistence.entity.User;
 import eg.gov.iti.jets.persistence.entity.aws.Ami;
 import eg.gov.iti.jets.persistence.entity.aws.SecurityGroup;
@@ -17,13 +17,20 @@ import java.util.Optional;
 
 @Service
 public class MapperUtilForApi {
+
     @Autowired
     private SecurityGroupDao securityGroupDao;
     @Autowired
     UserDao userDao;
 
     @Autowired
+    IntakeDao intakeDao;
+
+    @Autowired
     private BranchDao branchDao;
+
+    @Autowired
+    private TrainingProgramDao trainingProgramDao;
     @Autowired
     AwsGateway awsGateway;
 
@@ -52,9 +59,25 @@ public class MapperUtilForApi {
     }
 
 
-
     public Branch getBranchById(int id) {
         Optional<Branch> branch = branchDao.findById(id);
         return branch.orElse( null );
     }
+
+
+    public TrainingProgram getTrainingProgramById(int id) {
+        Optional<TrainingProgram> trainingProgram = trainingProgramDao.findById(id);
+        return trainingProgram.orElse( null );
+    }
+
+    public Intake getIntackById(int id) {
+        Optional<Intake> intake = intakeDao.findById(id);
+        System.out.println("##########################################################");
+        System.out.println(intake);
+        return intake.orElse( null );
+    }
+
+
+
+
 }
