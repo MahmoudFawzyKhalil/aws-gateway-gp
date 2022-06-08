b1f9aa9680ed30e157064b9369ebc782b46fcf9f
marwaayosiif <marwaayosiif@gmail.com>
2022-06-08 12:21:00 +0200

    user link edit

--- src/main/java/eg/gov/iti/jets/App.java
+++ src/main/java/eg/gov/iti/jets/App.java
@@ -258,47 +258,47 @@ public class App {
         SpringApplication.run( App.class, args );
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner( IntakeDao intakeDao, TrackDao trackDao, TrainingProgramDao trainingProgramDao, BranchDao branchDao, PrivilegeDao privilegeDao, SecurityGroupDao securityGroupDao, RoleDao roleDao, UserDao userDao, KeyPairDao keyPairDao, InstanceDao instanceDao, AmiDao amiDao, TemplateConfigurationDao templateConfigurationDao ) {
-
-        return ars -> {
-            if ( roleDao.findAllByExample( new Role( null, "STUDENT", null ) ).isEmpty() ) {
-
-                Privilege privilegeRead = new Privilege( null, PrivilegeName.READ, null );
-                Privilege privilegeManageTemplate = new Privilege( null, PrivilegeName.MANAGE_TEMPLATE, null );
-                Privilege privilegeCreateTerminateInstance = new Privilege( null, PrivilegeName.CREATE_TERMINATE_INSTANCE, null );
-                Privilege privilegeStartStopInstance = new Privilege( null, PrivilegeName.START_STOP_INSTANCE, null );
-                Privilege privilegeViewTemplates = new Privilege( null, PrivilegeName.VIEW_TEMPLATES, null );
-                privilegeRead = privilegeDao.save( privilegeRead );
-                privilegeViewTemplates = privilegeDao.save( privilegeViewTemplates );
-                privilegeStartStopInstance = privilegeDao.save( privilegeStartStopInstance );
-                privilegeCreateTerminateInstance = privilegeDao.save( privilegeCreateTerminateInstance );
-                privilegeManageTemplate = privilegeDao.save( privilegeManageTemplate );
-                Role supervisorRole = roleDao.save( new Role( null, "ROLE_SUPERVISOR", List.of( privilegeManageTemplate,
-                        privilegeCreateTerminateInstance,
-                        privilegeStartStopInstance,
-                        privilegeViewTemplates,
-                        privilegeRead ) ) );
-
-                Role instructorRole = roleDao.save( new Role( null, "ROLE_INSTRUCTOR", List.of( privilegeCreateTerminateInstance,
-                        privilegeStartStopInstance,
-                        privilegeViewTemplates,
-                        privilegeRead ) ) );
-
-                Role studentRole = roleDao.save( new Role( null, "ROLE_STUDENT", List.of( privilegeRead ) ) );
-                System.out.println( studentRole.getName() + " " + studentRole.getId() );
-                User supervisorUser = userDao.save(
-                        new User( null, "ashrf", "ashrf@g.com",
-                                "1234", supervisorRole, null, null, null, null, null ) );
-                User adminUser = userDao.save(
-                        new User( null, "marwa", "m@m.com",
-                                "1234", instructorRole, null, null, null, null, null ) );
-                User studentUser = userDao.save(
-                        new User( null, "hesham", "h@h.com",
-                                "1234", studentRole, null, null, null, null, null ) );
-                User studentUser2 = userDao.save(
-                        new User( null, "hossam", "ho@h.com",
-                                "1234", studentRole, null, null, null, null, null ) );
+//    @Bean
+//    CommandLineRunner commandLineRunner( IntakeDao intakeDao, TrackDao trackDao, TrainingProgramDao trainingProgramDao, BranchDao branchDao, PrivilegeDao privilegeDao, SecurityGroupDao securityGroupDao, RoleDao roleDao, UserDao userDao, KeyPairDao keyPairDao, InstanceDao instanceDao, AmiDao amiDao, TemplateConfigurationDao templateConfigurationDao ) {
+//
+//        return ars -> {
+//            if ( roleDao.findAllByExample( new Role( null, "STUDENT", null ) ).isEmpty() ) {
+//
+//                Privilege privilegeRead = new Privilege( null, PrivilegeName.READ, null );
+//                Privilege privilegeManageTemplate = new Privilege( null, PrivilegeName.MANAGE_TEMPLATE, null );
+//                Privilege privilegeCreateTerminateInstance = new Privilege( null, PrivilegeName.CREATE_TERMINATE_INSTANCE, null );
+//                Privilege privilegeStartStopInstance = new Privilege( null, PrivilegeName.START_STOP_INSTANCE, null );
+//                Privilege privilegeViewTemplates = new Privilege( null, PrivilegeName.VIEW_TEMPLATES, null );
+//                privilegeRead = privilegeDao.save( privilegeRead );
+//                privilegeViewTemplates = privilegeDao.save( privilegeViewTemplates );
+//                privilegeStartStopInstance = privilegeDao.save( privilegeStartStopInstance );
+//                privilegeCreateTerminateInstance = privilegeDao.save( privilegeCreateTerminateInstance );
+//                privilegeManageTemplate = privilegeDao.save( privilegeManageTemplate );
+//                Role supervisorRole = roleDao.save( new Role( null, "ROLE_SUPERVISOR", List.of( privilegeManageTemplate,
+//                        privilegeCreateTerminateInstance,
+//                        privilegeStartStopInstance,
+//                        privilegeViewTemplates,
+//                        privilegeRead ) ) );
+//
+//                Role instructorRole = roleDao.save( new Role( null, "ROLE_INSTRUCTOR", List.of( privilegeCreateTerminateInstance,
+//                        privilegeStartStopInstance,
+//                        privilegeViewTemplates,
+//                        privilegeRead ) ) );
+//
+//                Role studentRole = roleDao.save( new Role( null, "ROLE_STUDENT", List.of( privilegeRead ) ) );
+//                System.out.println( studentRole.getName() + " " + studentRole.getId() );
+//                User supervisorUser = userDao.save(
+//                        new User( null, "ashrf", "ashrf@g.com",
+//                                "1234", supervisorRole, null, null, null, null, null ) );
+//                User adminUser = userDao.save(
+//                        new User( null, "marwa", "m@m.com",
+//                                "1234", instructorRole, null, null, null, null, null ) );
+//                User studentUser = userDao.save(
+//                        new User( null, "hesham", "h@h.com",
+//                                "1234", studentRole, null, null, null, null, null ) );
+//                User studentUser2 = userDao.save(
+//                        new User( null, "hossam", "ho@h.com",
+//                                "1234", studentRole, null, null, null, null, null ) );
 //                DummyData.populateStaticDataForSmartBranch(studentRole, branchMangerRole, intakeDao, trackDao, trainingProgramDao, branchDao, privilegeDao, securityGroupDao, roleDao, userDao, keyPairDao, instanceDao, amiDao, templateConfigurationDao);
 //                DummyData.populateStaticDataForIsmailiaBranch(studentRole, branchMangerRole, intakeDao, trackDao, trainingProgramDao, branchDao, privilegeDao, securityGroupDao, roleDao, userDao, keyPairDao, instanceDao, amiDao, templateConfigurationDao);
 //                DummyData.populateStaticDataForMenofiaBranch(studentRole, branchMangerRole, intakeDao, trackDao, trainingProgramDao, branchDao, privilegeDao, securityGroupDao, roleDao, userDao, keyPairDao, instanceDao, amiDao, templateConfigurationDao);
@@ -326,10 +326,10 @@ public class App {
 //            branches.forEach( b -> System.out.println( b.getName() ) );
 //            System.out.println( "Finished Inserting" );
 //
-            }
+//            }
             ;
-        };
-    }
+//        };
+//    }
 
 }
 
