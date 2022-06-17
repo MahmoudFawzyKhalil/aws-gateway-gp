4816d56cae35c337ab750cc69672123a8002add5
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-06-17 15:25:49 +0200

    refactor privilege endpoint

--- src/main/java/eg/gov/iti/jets/api/resource/privilege/PrivilegeController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/privilege/PrivilegeController.java
@@ -4,6 +4,8 @@ import eg.gov.iti.jets.api.util.Mapper;
 import eg.gov.iti.jets.persistence.entity.Privilege;
 import eg.gov.iti.jets.service.management.PrivilegeManagement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,24 +18,24 @@ public class PrivilegeController {
     private final Mapper mapper;
 
     @GetMapping
-    public GetAllPrivilegesResponse getAllPrivileges(){
+    public ResponseEntity<?> getAllPrivileges(){
         GetAllPrivilegesResponse getPrivilegesResponse = new GetAllPrivilegesResponse();
         List<GetPrivilegeResponse> privileges = privilegeManagement.getAllPrivilege().stream().map(
                 mapper::privilegeToGetPrivilegeResponse
         ) .collect(Collectors.toList());
         getPrivilegesResponse.setPrivileges(privileges);
-        return getPrivilegesResponse;
+        return new ResponseEntity<>(getPrivilegesResponse, HttpStatus.OK);
     }
 
-    @PostMapping
-    public Boolean addPrivilege(@RequestBody AddPrivilegeRequest addPrivilegeRequest) {
-      return privilegeManagement.addPrivilege(mapper.addPrivilegeRequestToPrivilege(addPrivilegeRequest));
-    }
+//    @PostMapping
+//    public Boolean addPrivilege(@RequestBody AddPrivilegeRequest addPrivilegeRequest) {
+//      return privilegeManagement.addPrivilege(mapper.addPrivilegeRequestToPrivilege(addPrivilegeRequest));
+//    }
 
     @GetMapping("/{id}")
-    public GetPrivilegeResponse getPrivilege(@PathVariable("id") int id) {
+    public ResponseEntity<?> getPrivilege(@PathVariable("id") int id) {
         Privilege privilege = privilegeManagement.getPrivilegeById(id);
-        return mapper.privilegeToGetPrivilegeResponse(privilege);
+        return new ResponseEntity<>(mapper.privilegeToGetPrivilegeResponse(privilege), HttpStatus.OK);
     }
 
 }
