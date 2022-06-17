3515e559b53b4c13d49959774074ebcc36030a84
Abdelazizeldahshan <abdelazizeldahshan5@gmail.com>
2022-06-17 17:23:23 +0200

    get user details in aop to logging instance creation

--- src/main/java/eg/gov/iti/jets/service/aop/InstanceLoggingAspect.java
+++ src/main/java/eg/gov/iti/jets/service/aop/InstanceLoggingAspect.java
@@ -9,12 +9,15 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class InstanceLoggingAspect {
+
     private SecurityContext  securityContext;
+
     public InstanceLoggingAspect(){
         this.securityContext=SecurityContextHolder.getContext();
     }
@@ -22,10 +25,8 @@ public class InstanceLoggingAspect {
     @Before( "execution(* eg.gov.iti.jets.service.management.InstanceManagement.createInstance(eg.gov.iti.jets.persistence.entity.aws.Instance))" )
     public void aroundCreateInstance( JoinPoint joinPoint ) throws Throwable {
         System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        System.out.println(securityContext.getAuthentication().getName());
-//        Object[] arguments = invocation.getArgs();
-//        Instance timeToLiveInMinutes = (Instance) arguments[0];
-//        Object result = invocation.proceed();
+        var authentication = securityContext.getAuthentication();
+        System.out.println(((UserDetails) authentication.getPrincipal()).getUsername());
         System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
 
     }
