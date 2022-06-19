d53c7f65dfa6489c4d03bd5c8548db67c6a10e7a
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-18 17:25:22 -0700

    update in Track Management

--- src/main/java/eg/gov/iti/jets/service/management/impl/TrackManagementImpl.java
+++ src/main/java/eg/gov/iti/jets/service/management/impl/TrackManagementImpl.java
@@ -2,12 +2,11 @@ package eg.gov.iti.jets.service.management.impl;
 
 import eg.gov.iti.jets.persistence.dao.IntakeDao;
 import eg.gov.iti.jets.persistence.dao.TrackDao;
-import eg.gov.iti.jets.persistence.entity.Branch;
-import eg.gov.iti.jets.persistence.entity.Intake;
-import eg.gov.iti.jets.persistence.entity.Track;
-import eg.gov.iti.jets.persistence.entity.TrainingProgram;
-import eg.gov.iti.jets.service.management.CrudOperations;
+import eg.gov.iti.jets.persistence.entity.*;
+import eg.gov.iti.jets.service.exception.ResourceExistException;
+import eg.gov.iti.jets.service.exception.ResourceNotFoundException;
 import eg.gov.iti.jets.service.management.TrackManagement;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,23 +14,43 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class TrackManagementImpl implements TrackManagement {
 
     @Autowired
     TrackDao trackDao;
-    @Autowired
-    IntakeDao intakeDao;
 
     @Override
     public Track createTrack(Track track) {
 
-        return trackDao.save(track);
-    }
+            return trackDao.save(track);
+
+//    catch (Exception e) {
+//        throw new ResourceExistException("Track" + track.getName() + ", is already exist!");
+//    }
+}
 
     @Override
     public Track updateTrack(Track track) {
-        return trackDao.update(track);
+        try {
+            return trackDao.update(track);
+        }
+    catch (Exception e) {
+        throw new ResourceNotFoundException("Could not update role with id " + track.getId());
+        }
     }
+
+//    @Override
+//    public Track updateTrack(int id ) {
+//        try {
+//            Optional<Track> track = trackDao.findById(id);
+//
+//        }
+//        catch (Exception e) {
+//            throw new ResourceNotFoundException("Could not update role with id " + track.getId());
+//        }
+//    }
+
 
     @Override
     public List<Track> getAllTracks() {
@@ -40,7 +59,12 @@ public class TrackManagementImpl implements TrackManagement {
 
     @Override
     public Optional<Track> getTrackById(int id) {
-        return trackDao.findById(id);
+        try {
+            return trackDao.findById(id);
+        }
+        catch (Exception e) {
+            throw new ResourceNotFoundException("Could not update Track with id "+ id +" because its not found");
+        }
     }
 
 
