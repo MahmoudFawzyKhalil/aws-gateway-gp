90abe2a7a4f024a4aaafeaf368187b56cfbcb655
Hossamcs <hussamaddin516@gmail.com>
2022-06-20 15:31:02 +0200

    Dtos validation

--- src/main/java/eg/gov/iti/jets/api/resource/user/CreateUserRequest.java
+++ src/main/java/eg/gov/iti/jets/api/resource/user/CreateUserRequest.java
@@ -2,12 +2,21 @@ package eg.gov.iti.jets.api.resource.user;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class CreateUserRequest {
 //    private Integer id;
+
+    @NotBlank(message = "Username should not be empty or null")
+    @Size(min = 3, max = 14 , message = "User name length should be between 3 and 14 characters")
     private String username;
+    @NotBlank(message = "Email should not be empty or null")
+    @Email(message = "Email should be in this format example@example.example")
     private String email;
+    @NotBlank(message = "Password should not be empty or null")
     private String password;
     private RoleType role;
+    @NotNull(message = "Manager id should not be null")
     private Integer managerId;
 }
--- src/main/java/eg/gov/iti/jets/api/resource/user/UpdateUserRequest.java
+++ src/main/java/eg/gov/iti/jets/api/resource/user/UpdateUserRequest.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserRequest {
+
+    @NotBlank(message = "Username should not be empty or null")
+    @Size(min = 3, max = 14 , message = "User name length should be between 3 and 14 characters")
     private String username;
+    @NotBlank(message = "Email should not be empty or null")
+    @Email(message = "Email should be in this format example@example.example")
     private String email;
+    @NotBlank(message = "Password should not be empty or null")
     private String password;
     private RoleType role;
+    @NotNull(message = "Manager id should not be null")
     private Integer managerId;
 
 }
