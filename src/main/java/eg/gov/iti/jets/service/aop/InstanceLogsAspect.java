aeeaf7d38e0123dee5a5b9f174065d0bb67bee37
Abdelazizeldahshan <abdelazizeldahshan5@gmail.com>
2022-06-22 13:54:14 +0200

    edite 3 methode afterStartInstance and afterStopInstance and afterDeletenstance in InstanceLogsAspect

--- src/main/java/eg/gov/iti/jets/service/aop/InstanceLogsAspect.java
+++ src/main/java/eg/gov/iti/jets/service/aop/InstanceLogsAspect.java
@@ -8,6 +8,7 @@ import eg.gov.iti.jets.persistence.entity.aws.Instance;
 import eg.gov.iti.jets.persistence.entity.aws.InstanceLogs;
 import eg.gov.iti.jets.persistence.entity.enums.UserAction;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -24,7 +25,6 @@ public class InstanceLogsAspect {
 
     private final InstanceLogsDao instanceLogsDao;
     private final UserDao userDao;
-
     private final InstanceDao instanceDao;
 
     public InstanceLogsAspect(InstanceLogsDao instanceLogsDao, UserDao userDao, InstanceDao instanceDao) {
@@ -33,15 +33,11 @@ public class InstanceLogsAspect {
         this.instanceDao = instanceDao;
     }
 
-
-//    @AfterReturning("execution(* eg.gov.iti.jets.service.management.InstanceManagement.createInstance(eg.gov.iti.jets.persistence.entity.aws.Instance)), returning = instance")
-
     @AfterReturning(
             pointcut = "execution(* eg.gov.iti.jets.service.management.InstanceManagement.createInstance(eg.gov.iti.jets.persistence.entity.aws.Instance))",
             returning = "returnedInstance")
     public void afterCreateInstance(JoinPoint joinPoint, Object returnedInstance) throws Throwable {
         SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%");
         UserDetails userDetails = (UserDetails) securityContext.getAuthentication().getPrincipal();
         String userName = userDetails.getUsername();
         User user = userDao.findByUsername(userName).orElse(null);
@@ -51,40 +47,60 @@ public class InstanceLogsAspect {
         instanceLogs.setDateTime(LocalDateTime.now());
         instanceLogs.setActionMaker(user);
         instanceLogs.setAction(UserAction.CREATE_INSTANCE);
-        System.out.println(instanceLogs.toString());
-
         instanceLogsDao.save(instanceLogs);
-
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
     }
 
 
-    @Before("execution(* eg.gov.iti.jets.service.management.InstanceManagement.startInstance(..))")
-    public void beforeStartInstance(JoinPoint joinPoint) throws Throwable {
+    @AfterReturning("execution(* eg.gov.iti.jets.service.management.InstanceManagement.startInstance(java.lang.String))")
+    public void afterStartInstance(JoinPoint joinPoint) throws Throwable {
         SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        var authentication = securityContext.getAuthentication();
-        System.out.println(((UserDetails) authentication.getPrincipal()).getUsername());
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
+        UserDetails userDetails = (UserDetails) securityContext.getAuthentication().getPrincipal();
+        String userName = userDetails.getUsername();
+        User user = userDao.findByUsername(userName).orElse(null);
+        Instance example = new Instance();
+        example.setInstanceId((String) joinPoint.getArgs()[0]);
+        Instance instance = instanceDao.findAllByExample(example).get(0);
+        InstanceLogs instanceLogs = new InstanceLogs();
+        instanceLogs.setInstance(instance);
+        instanceLogs.setDateTime(LocalDateTime.now());
+        instanceLogs.setActionMaker(user);
+        instanceLogs.setAction(UserAction.START_INSTANCE);
+        instanceLogsDao.save(instanceLogs);
     }
 
 
-    @Before("execution(* eg.gov.iti.jets.service.management.InstanceManagement.stopInstance(..))")
-    public void beforeStopInstance(JoinPoint joinPoint) throws Throwable {
+    @AfterReturning("execution(* eg.gov.iti.jets.service.management.InstanceManagement.stopInstance(java.lang.String))")
+    public void afterStopInstance(JoinPoint joinPoint) throws Throwable {
         SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        var authentication = securityContext.getAuthentication();
-        System.out.println(((UserDetails) authentication.getPrincipal()).getUsername());
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
+        UserDetails userDetails = (UserDetails) securityContext.getAuthentication().getPrincipal();
+        String userName = userDetails.getUsername();
+        User user = userDao.findByUsername(userName).orElse(null);
+        Instance example = new Instance();
+        example.setInstanceId((String) joinPoint.getArgs()[0]);
+        Instance instance = instanceDao.findAllByExample(example).get(0);
+        InstanceLogs instanceLogs = new InstanceLogs();
+        instanceLogs.setInstance(instance);
+        instanceLogs.setDateTime(LocalDateTime.now());
+        instanceLogs.setActionMaker(user);
+        instanceLogs.setAction(UserAction.STOP_INSTANCE);
+        instanceLogsDao.save(instanceLogs);
     }
 
 
-    @Before("execution(* eg.gov.iti.jets.service.management.InstanceManagement.deleteInstance(..))")
-    public void beforeDeletenstance(JoinPoint joinPoint) throws Throwable {
+    @AfterReturning("execution(* eg.gov.iti.jets.service.management.InstanceManagement.deleteInstance(java.lang.String))")
+    public void afterDeletenstance(JoinPoint joinPoint) throws Throwable {
         SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        var authentication = securityContext.getAuthentication();
-        System.out.println(((UserDetails) authentication.getPrincipal()).getUsername());
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
+        UserDetails userDetails = (UserDetails) securityContext.getAuthentication().getPrincipal();
+        String userName = userDetails.getUsername();
+        User user = userDao.findByUsername(userName).orElse(null);
+        Instance example = new Instance();
+        example.setInstanceId((String) joinPoint.getArgs()[0]);
+        Instance instance = instanceDao.findAllByExample(example).get(0);
+        InstanceLogs instanceLogs = new InstanceLogs();
+        instanceLogs.setInstance(instance);
+        instanceLogs.setDateTime(LocalDateTime.now());
+        instanceLogs.setActionMaker(user);
+        instanceLogs.setAction(UserAction.TERMINATE_INSTANCE);
+        instanceLogsDao.save(instanceLogs);
     }
 }
