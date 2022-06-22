3f50bf0d7c48fbbc6934ce0375cdada49a9d49a5
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-22 13:49:16 -0700

    add @NotBlanck to Branch Request

--- src/main/java/eg/gov/iti/jets/api/resource/branch/BranchPutRequest.java
+++ src/main/java/eg/gov/iti/jets/api/resource/branch/BranchPutRequest.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BranchPutRequest {
+    @NotBlank(message = "must not be empty or null data")
     private String address;
     private String name;
     private boolean branchStatus;
