c165326fca6cd84d78b251ab82b8210ea5df2e33
AhmedOsama <ahmedosamam03@gmail.com>
2022-06-23 14:57:43 +0200

    Add date time format , find by user and date time

--- src/main/java/eg/gov/iti/jets/api/resource/InstanceLog/InstanceLogController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/InstanceLog/InstanceLogController.java
@@ -2,10 +2,12 @@ package eg.gov.iti.jets.api.resource.InstanceLog;
 
 import eg.gov.iti.jets.api.resource.ResponseMessage;
 import eg.gov.iti.jets.persistence.dao.InstanceLogsDao;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -25,9 +27,19 @@ public class InstanceLogController {
     }
 
     @GetMapping("user/{id}")
-    public ResponseEntity<ResponseMessage> getAllByUser(@RequestParam(required = false, defaultValue = "0") int pn, @RequestParam(required = false, defaultValue = "30") int ps, @PathVariable int id) {
-        List<InstanceLogProjection> instanceLogProjections = instanceLogsDao.findAllByActionMaker(id, pn, ps, InstanceLogProjection.class);
+    public ResponseEntity<ResponseMessage> getAllByUserAndDateTime(@RequestParam(required = false, defaultValue = "0") int pn, @RequestParam(required = false, defaultValue = "30") int ps,
+                                                                   @RequestParam(required = false, name = "from") LocalDateTime from,
+                                                                   @RequestParam(required = false, name = "to") LocalDateTime to,
+                                                                   @PathVariable int id) {
+        List<InstanceLogProjection> instanceLogProjections;
+        if (from != null && to != null) {
+            instanceLogProjections = instanceLogsDao.findAllByActionMakerAndDateTime(id, from, to, pn, ps, InstanceLogProjection.class);
+        } else {
+            instanceLogProjections = instanceLogsDao.findAllByActionMaker(id, pn, ps, InstanceLogProjection.class);
+        }
         ResponseMessage responseMessage = new ResponseMessage(HttpStatus.OK.value(), instanceLogProjections, null);
         return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
+
+
 }
