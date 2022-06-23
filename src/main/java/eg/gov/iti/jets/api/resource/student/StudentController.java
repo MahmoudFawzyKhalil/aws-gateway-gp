e9e0a6e45bac887983c43d1085b386372a680d02
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-23 10:51:32 -0700

    Add Student Controller

---
+++ src/main/java/eg/gov/iti/jets/api/resource/student/StudentController.java
@@ -0,0 +1,45 @@
+package eg.gov.iti.jets.api.resource.student;
+
+import eg.gov.iti.jets.api.resource.track.TrackResponse;
+import eg.gov.iti.jets.api.resource.track.TrackResponseList;
+import eg.gov.iti.jets.api.resource.user.UserResponse;
+import eg.gov.iti.jets.api.resource.user.UserResponseList;
+import eg.gov.iti.jets.api.util.Mapper;
+import eg.gov.iti.jets.persistence.entity.Track;
+import eg.gov.iti.jets.persistence.entity.User;
+import eg.gov.iti.jets.service.management.impl.UserManagementImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final UserManagementImpl userManagement;
+    private final Mapper mapper;
+
+    public StudentController(UserManagementImpl userManagement , Mapper mapper){
+        this.userManagement = userManagement;
+        this.mapper = mapper;
+    }
+
+//    @GetMapping
+//    public ResponseEntity<StudentResponseList> getUsers(){
+//        List<User> users = userManagement.getAllUsers();
+//        List<UserResponse> userResponses =  mapper.mapFromListOfUsersToListOfUserResponses(users);
+//        UserResponseList studentResponseList = new UserResponseList();
+//        for(UserResponse response : userResponses){
+//            studentResponseList.getUserResponsesList().add(response);
+//        }
+//        return new ResponseEntity<>( studentResponseList , HttpStatus.OK );
+//    }
+
+}
+
+
