f61d81a9ecbddae23b466d6b7b1361d1b88b2151
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-23 11:16:01 -0700

    Add implementation of method Get All student

--- src/main/java/eg/gov/iti/jets/service/management/impl/StudentManagementImpl.java
+++ src/main/java/eg/gov/iti/jets/service/management/impl/StudentManagementImpl.java
@@ -1,9 +1,13 @@
 package eg.gov.iti.jets.service.management.impl;
 
+import eg.gov.iti.jets.persistence.dao.TrackDao;
+import eg.gov.iti.jets.persistence.dao.UserDao;
+import eg.gov.iti.jets.persistence.entity.Track;
 import eg.gov.iti.jets.persistence.entity.User;
 import eg.gov.iti.jets.service.management.StudentManagement;
 import eg.gov.iti.jets.service.management.UserManagement;
 import eg.gov.iti.jets.service.model.UserAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,8 +15,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import java.util.List;
 
 public class StudentManagementImpl implements UserDetailsService, StudentManagement {
+
+    @Autowired
+    UserDao userDao;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
+    }
+
+    @Override
+    public List<User> getAllStudent() {
+        return userDao.findAll();
     }
 }
