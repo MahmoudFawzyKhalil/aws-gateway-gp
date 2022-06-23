a82c77b2e5f3c69f88115a431e151822bd39a2c6
Abdelazizeldahshan <abdelazizeldahshan5@gmail.com>
2022-06-23 13:18:19 +0200

    edite RoleDao to add find role by name

--- src/main/java/eg/gov/iti/jets/persistence/dao/RoleDao.java
+++ src/main/java/eg/gov/iti/jets/persistence/dao/RoleDao.java
@@ -4,7 +4,6 @@ import eg.gov.iti.jets.persistence.entity.Role;
 import java.util.Optional;
 
 public interface RoleDao extends GenericCrudDao<Role,Integer> {
-
     Optional<Role> findRoleByName(String name);
 }
 
