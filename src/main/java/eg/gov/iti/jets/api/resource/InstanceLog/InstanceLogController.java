c43bca9a99de18ab681cfbeadc04f868e2546369
AhmedOsama <ahmedosamam03@gmail.com>
2022-06-23 13:51:23 +0200

    get all InstanceLogs for user

--- src/main/java/eg/gov/iti/jets/api/resource/InstanceLog/InstanceLogController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/InstanceLog/InstanceLogController.java
@@ -4,10 +4,7 @@ import eg.gov.iti.jets.api.resource.ResponseMessage;
 import eg.gov.iti.jets.persistence.dao.InstanceLogsDao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,13 @@ public class InstanceLogController {
     @GetMapping
     public ResponseEntity<ResponseMessage> getAll(@RequestParam(required = false, defaultValue = "0") int pn, @RequestParam(required = false, defaultValue = "30") int ps) {
         List<InstanceLogProjection> instanceLogProjections = instanceLogsDao.findAll(pn, ps, InstanceLogProjection.class);
+        ResponseMessage responseMessage = new ResponseMessage(HttpStatus.OK.value(), instanceLogProjections, null);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+    }
+
+    @GetMapping("user/{id}")
+    public ResponseEntity<ResponseMessage> getAllByUser(@RequestParam(required = false, defaultValue = "0") int pn, @RequestParam(required = false, defaultValue = "30") int ps, @PathVariable int id) {
+        List<InstanceLogProjection> instanceLogProjections = instanceLogsDao.findAllByActionMaker(id, pn, ps, InstanceLogProjection.class);
         ResponseMessage responseMessage = new ResponseMessage(HttpStatus.OK.value(), instanceLogProjections, null);
         return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
