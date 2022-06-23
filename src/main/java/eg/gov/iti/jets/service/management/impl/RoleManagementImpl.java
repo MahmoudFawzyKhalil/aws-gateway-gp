1c1769f6e194f96e9d087c757c426e15968324a3
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-06-23 15:03:02 +0200

    refactor role exceptions

--- src/main/java/eg/gov/iti/jets/service/management/impl/RoleManagementImpl.java
+++ src/main/java/eg/gov/iti/jets/service/management/impl/RoleManagementImpl.java
@@ -1,14 +1,18 @@
 package eg.gov.iti.jets.service.management.impl;
 
 import eg.gov.iti.jets.persistence.dao.RoleDao;
+import eg.gov.iti.jets.persistence.entity.Privilege;
 import eg.gov.iti.jets.persistence.entity.Role;
+import eg.gov.iti.jets.service.exception.ResourceConstraintsViolationException;
 import eg.gov.iti.jets.service.exception.ResourceExistException;
 import eg.gov.iti.jets.service.exception.ResourceNotFoundException;
 import eg.gov.iti.jets.service.management.RoleManagement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +23,9 @@ public class RoleManagementImpl implements RoleManagement {
     public Role addRole( Role role ) {
        try {
            return roleDao.save(role);
-       }catch (Exception e) {
-           throw new ResourceExistException("Role with name " + role.getName() + ", is already exist!");
+       } catch (Exception e) {
+           throw new ResourceConstraintsViolationException("Could not create role with id " + role.getId()+ ", privileges" + role.getPrivileges()
+                   .stream().map(Privilege::getId).collect(Collectors.toList()));
        }
     }
 
