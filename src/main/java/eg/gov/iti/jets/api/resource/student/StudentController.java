9e679796544f6cd8cb4a806568361a7f4e928bff
marwa abdulkarim <marwaadel904@gmail.com>
2022-06-23 12:53:52 -0700

    add get all student to Student Controller

--- src/main/java/eg/gov/iti/jets/api/resource/student/StudentController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/student/StudentController.java
@@ -1,108 +1,40 @@
 package eg.gov.iti.jets.api.resource.student;
 
-import eg.gov.iti.jets.api.resource.track.TrackResponse;
-import eg.gov.iti.jets.api.resource.track.TrackResponseList;
-import eg.gov.iti.jets.api.resource.user.UserResponse;
-import eg.gov.iti.jets.api.resource.user.UserResponseList;
 import eg.gov.iti.jets.api.util.Mapper;
-import eg.gov.iti.jets.persistence.entity.Track;
 import eg.gov.iti.jets.persistence.entity.User;
-import eg.gov.iti.jets.service.management.impl.UserManagementImpl;
+import eg.gov.iti.jets.service.management.StudentManagement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
 
-    private final UserManagementImpl userManagement;
+    private final StudentManagement studentManagement;
     private final Mapper mapper;
 
-    public StudentController(UserManagementImpl userManagement , Mapper mapper){
-        this.userManagement = userManagement;
+    public StudentController(StudentManagement studentManagement , Mapper mapper){
+        this.studentManagement = studentManagement;
         this.mapper = mapper;
     }
 
-//    @GetMapping
-//    public ResponseEntity<StudentResponseList> getUsers(){
-//        List<User> users = userManagement.getAllUsers();
-//        List<UserResponse> userResponses =  mapper.mapFromListOfUsersToListOfUserResponses(users);
-//        UserResponseList studentResponseList = new UserResponseList();
-//        for(UserResponse response : userResponses){
-//            studentResponseList.getUserResponsesList().add(response);
-//        }
-//        return new ResponseEntity<>( studentResponseList , HttpStatus.OK );
-//    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @PostMapping
-    public ResponseEntity createStudent(@RequestBody StudentRequest studentRequest){
-
-
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<StudentResponseList> getStudent(){
+        List<User> student = studentManagement.getAllStudent();
+        List<StudentResponse> studentResponses =  mapper.mapFromListOfStudentToListOfStudentResponses(student);
+        StudentResponseList studentResponseList = new StudentResponseList();
+        for ( StudentResponse response : studentResponses ) {
+            studentResponseList.getStudentResponsesList().add( response );
+        }
+        return new ResponseEntity<>( studentResponseList , HttpStatus.OK );
     }
+
+
 }
 
 
