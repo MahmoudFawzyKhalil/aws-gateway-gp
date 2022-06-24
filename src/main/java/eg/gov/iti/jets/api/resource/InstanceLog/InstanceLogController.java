e57f5ea83a242a28dc74abc579e5c9c7da7e547f
AhmedOsama <ahmedosamam03@gmail.com>
2022-06-24 13:44:27 +0200

    solve page number -> start from 1 not 0

--- src/main/java/eg/gov/iti/jets/api/resource/InstanceLog/InstanceLogController.java
+++ src/main/java/eg/gov/iti/jets/api/resource/InstanceLog/InstanceLogController.java
@@ -22,20 +22,20 @@ public class InstanceLogController {
     }
 
     @GetMapping
-    public ResponseEntity<ResponseMessage> getAll(@RequestParam(required = false, defaultValue = "0") int pn, @RequestParam(required = false, defaultValue = "30") int ps) {
-        List<InstanceLogProjection> instanceLogProjections = instanceLogsDao.findAll(pn, ps, InstanceLogProjection.class);
+    public ResponseEntity<ResponseMessage> getAll(@RequestParam(required = false, defaultValue = "1") int pn, @RequestParam(required = false, defaultValue = "30") int ps) {
+        List<InstanceLogProjection> instanceLogProjections = instanceLogsDao.findAll(pn-1, ps, InstanceLogProjection.class);
         ResponseMessage responseMessage = new ResponseMessage(HttpStatus.OK.value(), instanceLogProjections, null);
         return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
 
     @GetMapping("user/{id}")
-    public ResponseEntity<ResponseMessage> getAllByUserAndDateTime(@RequestParam(required = false, defaultValue = "0") int pn, @RequestParam(required = false, defaultValue = "30") int ps,
+    public ResponseEntity<ResponseMessage> getAllByUserAndDateTime(@RequestParam(required = false, defaultValue = "1") int pn, @RequestParam(required = false, defaultValue = "30") int ps,
                                                                    @RequestParam(required = false, name = "from") LocalDateTime from,
                                                                    @RequestParam(required = false, name = "to") LocalDateTime to,
                                                                    @PathVariable int id) {
         List<InstanceLogProjection> instanceLogProjections;
         if (from != null && to != null) {
-            instanceLogProjections = instanceLogsDao.findAllByActionMakerAndDateTime(id, from, to, pn, ps, InstanceLogProjection.class);
+            instanceLogProjections = instanceLogsDao.findAllByActionMakerAndDateTime(id, from, to, pn-1, ps, InstanceLogProjection.class);
         } else {
             instanceLogProjections = instanceLogsDao.findAllByActionMaker(id, pn, ps, InstanceLogProjection.class);
         }
