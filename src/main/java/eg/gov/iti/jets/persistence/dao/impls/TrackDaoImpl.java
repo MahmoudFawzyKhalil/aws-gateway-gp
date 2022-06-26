7abf15109570148677ae913b36a79a05680a2599
AhmedOsama <ahmedosamam03@gmail.com>
2022-06-26 18:42:43 +0200

    remove user from track dao

--- src/main/java/eg/gov/iti/jets/persistence/dao/impls/TrackDaoImpl.java
+++ src/main/java/eg/gov/iti/jets/persistence/dao/impls/TrackDaoImpl.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -74,6 +75,12 @@ public class TrackDaoImpl implements TrackDao {
 
     @Override
     public <C> List<C> geAllByTrackId(int intakeId, Class<C> projection) {
-        return trackRepo.findAllByIntake_Id(intakeId,projection);
+        return trackRepo.findAllByIntake_Id(intakeId, projection);
+    }
+
+    @Override
+    @Transactional
+    public int removeUserFromTrack(int userId, int trackId) {
+        return trackRepo.removeUserFromTrack(userId, trackId);
     }
 }
